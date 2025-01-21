c7403b65901bbbb0550b4ce6219bd0a0b3496818
Luke Selden <luke.selden@vbrick.com>
2025-01-21 17:12:38 -0500

    add config object type interface

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,37 @@
 const response = await fetch('./config.json')
-const config = await response.json()
+const config: PluginConfig = await response.json()
+
+export interface PluginConfig {
+  /**
+   * Vbrick Rev configuration
+   */
+  vbrick: {
+    /**
+     * Vbrick Rev URL
+     * @example https://company.rev.vbrick.com
+     */
+    url: string;
+    /**
+     * OAuth Client ID
+     * @see https://revdocs.vbrick.com/docs/create-an-api-key
+     */
+    client_id: string;
+    /**
+     * OAuth redirect uri. This must match the Vbrick Rev settings and plugin URI exactly
+     * @example https://my.pexip.instance/subpath/redirect
+     */
+    redirect_uri: string;
+  }
+  /**
+   * Pexip infinity settings
+   */
+  infinity: {
+    /**
+     * Pexip sip domain
+     * @example my.pexip.instance
+     */
+    sip_domain: string;
+  }
+}
 
 export { config }
