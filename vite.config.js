5ae6310cfd1dfdd01fa82697ae62527835b74471
Luke Selden <luke.selden@vbrick.com>
2025-01-23 23:58:21 -0500

    allow setting config file during development in vite.json

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
+import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
 
-import config from './vite.json'
+import devConfig from './vite.json'
 
-export default {
+const config = {
+  infinityUrl: "https://example.com",
+  port: 5173,
+  brandingPath: "/local-plugin",
+  pluginName: "vbrick",
+  ...devConfig
+};
+
+if (!config.infinityUrl || config.infinityUrl === "https://example.com") {
+  throw new Error('FATAL: Update vite.json with a valid infinityUrl value');
+}
+
+const pluginPath = `${config.brandingPath}/branding/plugins/${config.pluginName}`;
+
+export default defineConfig({
   base: './',
   build: {
     target: 'esnext',
@@ -17,6 +32,20 @@ export default {
     port: config.port,
     open: config.brandingPath + '/',
     proxy: {
+      [`${pluginPath}/config.json`]: {
+        bypass(req, res, options) {
+          res.statusCode = 200;
+          res.setHeader('content-type', 'application/json');
+          res.write(JSON.stringify(config));
+          res.end();
+          return req.url;
+        }
+      },
+      [`^${pluginPath}`]: {
+        bypass(req, res, options) {
+          return req.url.replace(pluginPath, '');
+        }
+      },
       [config.brandingPath]: {
         target: config.infinityUrl,
         changeOrigin: true,
@@ -26,10 +55,10 @@ export default {
         target: config.infinityUrl,
         changeOrigin: true,
         secure: false
-      }
+      },
     }
   },
   plugins: [
     mkcert()
   ]
-}
+})
